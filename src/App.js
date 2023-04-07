812ea48e2d065a9578e276f291511d9252845e07
Jasbar Legaspina <brailjazz@gmail.com>
2023-04-07 11:32:10 +0800

    Setting up the app code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import React from 'react';
 import HomePage from "./pages/HomePage"
-
 import About from "./pages/About"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
